3e73aad62141dc575d323fd5d6480ad4a56928e0
sen2y <pse6704@naver.com>
2023-05-22 12:02:30 +0900

    feature/footer생성

--- layouts/appLayout.tsx
+++ layouts/appLayout.tsx
@@ -8,10 +8,9 @@ interface IProps {
 // 전체적인 layout
 // css 수정해서 중앙 정렬하기
 // children( 모든 컴포넌트들 )이 표시되는 영역 설정하기 (중앙 정렬, px 추가)
-
 export default function AppLayout({ children }: IProps) {
   return (
-    <div className="relative flex flex-col">
+    <div className="relative flex flex-col items-center justify-center">
       <Header />
       {children}
       <Footer />
