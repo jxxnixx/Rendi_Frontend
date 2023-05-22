f1a85177cfce80653e9915b7a83681f9646df395
sen2y <pse6704@naver.com>
2023-05-22 15:23:44 +0900

    로그인 페이지 위치 정렬

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -168,23 +168,24 @@ function LogIn() {
   return (
     <>
       <Layout>
+        <NavBar />
         <Head>
           <title>LogIn</title>
         </Head>
         <div className=" flex flex-col items-center">
-          <div className="absolute top-[109px] text-center">
+          <div className="relative top-[241px] text-center">
             <p className="text-4xl font-semibold text-black">로그인</p>
           </div>
-          <div className="absolute top-[173px] text-center">
+          <div className="absolute top-[305px] text-center">
             <p className="text-lg text-[#666]">
               Rendi만의 지능형 AI 검색을 경험해보세요!
             </p>
           </div>
         </div>
         ;
-        <div className="flex justify-center items-center h-screen">
+        <div className="flex justify-center items-center top-[400px] h-screen">
           <form
-            className="flex flex-col items-center gap-6 p-0 w-448 h-1017"
+            className=" flex flex-col items-center mt-[-60px] gap-1 p-0 w-448 h-1017"
             onSubmit={handleSubmit(submitForm)}
           >
             <Input
@@ -207,18 +208,18 @@ function LogIn() {
               autoComplete="off"
             />
 
-            <Link href="/signUp" legacyBehavior>
-              <button className="bg-white text-gray-600 text-base">
-                Rendi가 처음이신가요?
-              </button>
-            </Link>
             <div className="flex text-center justify-center">
               <SubmitButton
                 type="submit"
                 text="로그인"
-                classname="flex justify-center items-center h-screen"
+                className="flex justify-center items-center h-screen"
               />
             </div>
+            <Link href="/auth/signUp" legacyBehavior>
+              <button className="mt-[100px] bg-white text-gray-600 text-base">
+                Rendi가 처음이신가요?
+              </button>
+            </Link>
           </form>
         </div>
         {/* </Layout> */}
