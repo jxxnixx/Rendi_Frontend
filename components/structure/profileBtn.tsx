c454e53ba6dcfb208f48c280458212fa81fcd548
sen2y <pse6704@naver.com>
2023-05-26 15:04:33 +0900

    feature/MyPage/상단부분-각 연결페이지는 /auth/profile아래

--- components/structure/profileBtn.tsx
+++ components/structure/profileBtn.tsx
@@ -4,16 +4,14 @@ import { MyPage } from "../icons";
 export default function ProfileBtn() {
   return (
     <>
-      <Link href="/auth/signUp" legacyBehavior>
-        <button className="w-[50px] h-[30px]">
-          <p className="w-[50px] h-[30px] absolute left-[945px] top-[5px] opacity-75 text-xs font-medium text-left text-[#666666]">
-            회원가입
-          </p>
-        </button>
-      </Link>
-      <p className="absolute left-[935px] top-[4px] opacity-75 text-xs font-medium text-left text-[#666]">
-        |
-      </p>
+      <button className="w-[18px] h-[18px] absolute left-[870px] top-[5px] opacity-75 text-xs font-medium text-left text-[#666666]">
+        <Link href="/auth/profile" legacyBehavior>
+          <a className="text-[#666]">
+            <MyPage />
+          </a>
+        </Link>
+      </button>
+
       <Link href="/auth/login" legacyBehavior>
         <button className="w-[40px] h-[30px]">
           <p className="w-[40px] h-[30px] absolute left-[890px] top-[5px] opacity-75 text-xs font-medium text-left text-[#666666]">
@@ -21,14 +19,18 @@ export default function ProfileBtn() {
           </p>
         </button>
       </Link>
-      {/* 밑에는 연주가 사람 아이콘 추가했던거 */}
-      <button className="w-[18px] h-[18px] absolute left-[870px] top-[5px] opacity-75 text-xs font-medium text-left text-[#666666]">
-        <Link href="/profile" legacyBehavior>
-          <a className="text-[#666]">
-            <MyPage />
-          </a>
-        </Link>
-      </button>
+
+      <p className="absolute left-[935px] top-[4px] opacity-75 text-xs font-medium text-left text-[#666]">
+        |
+      </p>
+
+      <Link href="/auth/signUp" legacyBehavior>
+        <button className="w-[50px] h-[30px]">
+          <p className="w-[50px] h-[30px] absolute left-[945px] top-[5px] opacity-75 text-xs font-medium text-left text-[#666666]">
+            회원가입
+          </p>
+        </button>
+      </Link>
     </>
   );
 }
---
+++ pages/auth/profile/likedMarket.tsx
@@ -0,0 +1,10 @@
+import { SubmitHandler, useForm } from "react-hook-form";
+import SubmitBtn from "@/components/function/submitBtn";
+import Input from "@/components/function/input";
+import Layout from "@/layouts/layout";
+import Head from "next/head";
+import Link from "next/link";
+
+function LikedMarket() {}
+
+export default LikedMarket;
