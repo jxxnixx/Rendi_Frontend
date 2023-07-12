19f91e21b16d227698ee9e9868db289dc8ed5de4
sen2y <pse6704@naver.com>
2023-07-12 19:44:09 +0900

    카메라 검색/배너추가

--- components/structure/banner.tsx
+++ components/structure/banner.tsx
@@ -1,7 +1,35 @@
+import React from "react";
+import { Carousel } from "antd";
+import { LeftCircleOutlined, RightCircleOutlined } from "@ant-design/icons";
+
 export default function Banner() {
   return (
-    <div className="w-[1040px] h-[362px] relative">
-      <img src="banner.png" className="w-[1040px] h-[362px] object-cover" />
+    <div className="w-[1040px] h-[362px] ">
+      {/* <img src="banner.png" className="w-[1040px] h-[362px] object-cover" /> */}
+
+      <Carousel autoplay draggable>
+        <div>
+          <img src="banner.png" className="w-[1040px] h-[362px] object-cover" />
+        </div>
+        <div>
+          <div className="flex justify-center items-center w-[1040px] h-[362px] relative overflow-hidden bg-[#ffe9ec]/90">
+            <div className="flex justify-center items-center w-[1000px] h-[287px] ">
+              <div className="flex h-[186px]  flex-col">
+                <p className="text-[18px] text-[#666666] font-medium">
+                  Rendi에 처음 오셨나요?
+                </p>
+                <p className="text-[45px] font-bold  text-shadow-[0px 4px 4px rgba(0,0,0,0.25)">
+                  test page
+                </p>
+              </div>
+              <div className="flex-1 h-[186px] bg-black"></div>
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-center items-center w-[1040px] h-[362px] relative overflow-hidden bg-orange-200">
+          <h1 className="color-white line-height-[362px]">slide1</h1>
+        </div>
+      </Carousel>
     </div>
   );
 }
--- components/structure/searchBar.tsx
+++ components/structure/searchBar.tsx
@@ -1,6 +1,6 @@
-//지우세욤 연쥬밥오ㅜ
 import React, { useState, useRef, useEffect } from "react";
 import { Camera, Search } from "../icons";
+import { Button, Upload } from "antd";
 
 export default function SearchBar() {
   //Ref로 사각형, input dom요소 참조
@@ -11,6 +11,7 @@ export default function SearchBar() {
 
   // 검색창 하단 사각형 표시 여부 관리
   const [showSquare, setShowSquare] = useState(false);
+  const [showUpload, setShowUpload] = useState(false);
 
   // 버튼 클릭 여부 관리 recent , popular 둘 중 하나로 결정
   const [showContent, setShowContent] = useState("recent");
@@ -41,6 +42,10 @@ export default function SearchBar() {
     setShowSquare(true);
   };
 
+  const handleCameraClick = () => {
+    setShowUpload(true);
+  };
+
   // 클릭된 button 구별, 관리
   const handleTabClick = (tab: string) => {
     setShowContent(tab);
@@ -49,6 +54,10 @@ export default function SearchBar() {
   // 닫기 button
   const handleCloseClick = () => {
     setShowSquare(false);
+  };
+
+  const handleCameraCloseClick = () => {
+    setShowUpload(false);
   };
 
   return (
@@ -61,13 +70,14 @@ export default function SearchBar() {
       </label>
       {/* div 수정 */}
       <div className="relative">
-        {!showSquare ? (
+        {!showSquare && !showUpload ? (
           <input
             ref={inputRef}
             type="search"
             id="default-search"
-            className="block w-[679px] h-[46px] px-[20px] rounded-[50px] bg-white border-2 border-mc focus:outline-none focus:ring-0"
-            placeholder="검색어를 입력하세요"
+            className="block w-[679px] h-[46px] px-[20px] rounded-[50px] bg-white border-2 border-[#FC435A] focus:outline-none focus:ring-0"
+            // placeholder=!showSquare?"검색어를 입력하세요":"사진을 업로드해주세요"
+            placeholder="검색어를 입력하세요."
             onClick={handleInputClick}
           />
         ) : (
@@ -75,15 +85,50 @@ export default function SearchBar() {
             ref={inputRef}
             type="search"
             id="default-search"
-            className="block w-[679px] h-[46px] px-[20px] rounded-t-[23px] bg-white border-2 border-mc focus:outline-none focus:ring-0 "
-            placeholder="검색어를 입력하세요"
+            className="block w-[679px] h-[46px] px-[20px] rounded-t-[23px] bg-white border-2 border-[#FC435A] focus:outline-none focus:ring-0 "
+            placeholder={
+              !showSquare && showUpload
+                ? "사진을 업로드해주세요."
+                : "검색어를 입력하세요."
+            }
             onClick={handleInputClick}
           />
+        )}
+        {showUpload && (
+          <div className="absolute top-[46px] left-[0px] w-[679px] h-[456px] bg-white border-2 border-[#FC435A]  rounded-b-[23px] border-t-0">
+            <div className="flex  flex-col justify-center items-center w-[679px] h-[410px]">
+              <div className=" w-[600px] mt-[10px] p-[20px] overflow-y-auto">
+                <span className="flex m-[20px] justify-center text-sm  font-medium text-gray-900 ">
+                  여기로 이미지를 업로드해주세요.
+                </span>
+                <div>
+                  <Upload.Dragger
+                    multiple
+                    listType="picture"
+                    accept=".jpg,.png, .jpeg"
+                    //action 속성은 <Upload> 컴포넌트가 파일을 업로드할 서버 쪽 스크립트 또는 API 엔드포인트를 가리키는 URL을 설정합니다. 업로드된 파일은 이 URL로 전송되어 서버 쪽에서 처리됩니다.
+                    action={""}
+                  >
+                    <Button>upload</Button>
+                  </Upload.Dragger>
+                </div>
+              </div>
+            </div>
+            <div className="w-[679px] h-12 absolute left-0 top-[408px] overflow-hidden">
+              <button
+                id="close"
+                className="w-[463px] h-[37px] absolute left-[108px] top-1.5 text-sm font-medium text-center text-[#666]"
+                onClick={handleCameraCloseClick}
+              >
+                닫기
+              </button>
+            </div>
+          </div>
         )}
 
         {showSquare && (
           <div ref={squareRef} className="w-[679px] h-[456px] relative z-0">
-            <div className="flex justify-start items-start w-[679px] h-[456px] absolute left-0 top-0 rounded-b-[23px] bg-white border-x-2 border-b-2 border-mc">
+            <div className="flex justify-start items-start w-[679px] h-[456px] absolute left-0 top-0 rounded-b-[23px] bg-white border-x-2 border-b-2 border-[#fc435a]">
               <div
                 className={`w-[676px] h-12 absolute ${
                   showContent === "recent" ? "left-[0px]" : "right-[0px]"
@@ -133,7 +178,7 @@ export default function SearchBar() {
                     id="recent"
                     className={`w-1/2 h-12 items-center justify-center content-center ${
                       showContent === "recent"
-                        ? "text-mc border-b-2 border-mc"
+                        ? "text-[#FC435A] border-b-2 border-[#FC435A]"
                         : ""
                     }`}
                     onClick={() => handleTabClick("recent")}
@@ -145,7 +190,7 @@ export default function SearchBar() {
                     id="popular"
                     className={`w-1/2 h-12 items-center justify-center ${
                       showContent === "popular"
-                        ? "text-mc border-b-2 border-mc"
+                        ? "text-[#FC435A] border-b-2 border-[#FC435A]"
                         : ""
                     }`}
                     onClick={() => handleTabClick("popular")}
@@ -178,8 +223,10 @@ export default function SearchBar() {
         )}
         {/* 버튼 두개 위치 수정 */}
         <button
-          type="submit"
-          className="absolute inset-y-0 right-[45px] top-[11px] flex items-center justify-center w-[25px] h-[25px] p-0 bg-transparent focus:ring-0 focus:outline-none text-gray-500 dark:text-gray-400"
+          type="button"
+          className="absolute inset-y-0 right-[45px] top-[11px] flex items-center justify-center w-[25px] h-[25px] p-0 bg-transparent focus:ring-0 focus:outline-none text-gray-500 dark:text-gray-400
+          "
+          onClick={handleCameraClick}
         >
           <Camera />
         </button>
