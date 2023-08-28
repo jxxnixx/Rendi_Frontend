e906816309b04f495cfeab65721a73d0e32891ad
sen2y <pse6704@naver.com>
2023-08-28 18:36:07 +0900

    메인 css 문제 해결

--- pages/main/index.tsx
+++ pages/main/index.tsx
@@ -1,27 +1,43 @@
 import HoriCategory from "@/components/category/horiCategory";
 import Banner from "@/components/structure/banner";
 import Layout from "@/layouts/layout";
+import type { NextPage } from "next";
 import Head from "next/head";
+import { useRouter } from "next/router";
 import Items from "@/components/product/items";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
+import { useScreenSize } from "@/libs/client/useScreen";
 
-export default function Main() {
+const Home: NextPage = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const accessToken = localStorage.getItem("accessToken");
+    if (accessToken) {
+      router.push("/main");
+    }
+  }, []);
+
   return (
     <Layout>
       <Head>
-        <title>Main</title>
+        <title>Home</title>
       </Head>
-      <div className="relative mt-[135px] bg-slate-200">
+      <div className="relative mt-[135px] bg-slate-200 mobile:mt-[85px]">
         <div className="bg-white">
           <div className="flex justify-center">
             <Banner />
           </div>
-          <div className="flex justify-center">
-            <HoriCategory />
+          <div className="flex justify-center mobile:w-full">
+            {/* 스크롤 가능한 영역 */}
+            <div className="overflow-x-scroll scrollbar-hide">
+              <HoriCategory />
+            </div>
           </div>
 
           <div className="flex justify-center">
-            <div className="flex-row w-[1040px]">
+            <div className="flex-row w-[1040px] mobile:w-full">
               <div className="flex justify-between text-[12pt] font-medium text-black">
                 <p className="ml-[30px] mt-[10px]">이번주 신제품</p>
 
@@ -41,4 +57,6 @@ export default function Main() {
       </div>
     </Layout>
   );
-}
+};
+
+export default Home;
