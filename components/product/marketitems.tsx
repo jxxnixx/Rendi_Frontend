233d649c3fdc465596d7e8bdda1590ced61b686b
sen2y <pse6704@naver.com>
2023-09-16 23:57:11 +0900

    market 구현중

--- components/product/marketitems.tsx
+++ components/product/marketitems.tsx
@@ -1,10 +1,8 @@
 import React, { useEffect, useState } from "react";
-import MarketItem from "./item"; // 여기서 "MarketItem"으로 수정
 import dummyData from "./dummyData.json";
 import { Product } from "@/components/product/DataTypes";
 import { useRouter } from "next/router";
 import { useScreenSize } from "@/libs/client/useScreen";
-import { itemsApi } from "@/libs/api";
 import Markeitem from "./markeitem";
 
 interface ItemsProps {
@@ -12,75 +10,11 @@ interface ItemsProps {
   itemsToShow?: Product[];
 }
 
-// type ClickCounts = { [productId: number]: number };
-// type LastClickTimes = { [productId: number]: number | null };
-
 export default function MarketItems({ itemsPerPage, itemsToShow }: ItemsProps) {
   const router = useRouter();
   const screen = useScreenSize();
   const allItems: Product[] = dummyData;
   const itemsToDisplay = itemsToShow || allItems.slice(0, itemsPerPage);
-  // 만약 itemsToShow가 제공되지 않았다면, 처음 itemsPerPage개의 상품만 보여줍니다.
-
-  // const [clickCounts, setClickCounts] = useState<ClickCounts>({});
-  // const [lastClickTimes, setLastClickTimes] = useState<LastClickTimes>({});
-  // const [intervalId, setIntervalId] = useState<NodeJS.Timeout | null>(null);
-
-  // const updateClickCount = (productId: number, clickCount: number) => {
-  //   setClickCounts((prevClickCounts) => ({
-  //     ...prevClickCounts,
-  //     [productId]: clickCount,
-  //   }));
-  //   console.log(productId + ":" + clickCount);
-  // };
-
-  // const updateLastClickTime = (productId: number, clickTime: number | null) => {
-  //   setLastClickTimes((prevLastClickTimes) => ({
-  //     ...prevLastClickTimes,
-  //     [productId]: clickTime,
-  //   }));
-  // };
-
-  // const sendClickCountsToBackend = async () => {
-  //   try {
-  //     for (const productId in clickCounts) {
-  //       const productIdn = parseInt(productId); // 문자열을 숫자로 변환
-  //       const clickCount = clickCounts[productId];
-  //       console.log(productIdn);
-  //       console.log(clickCount);
-  //       console.log(clickCounts);
-
-  //       const hitsResponse = await itemsApi.updateHits(productIdn, clickCount);
-  //       console.log(hitsResponse);
-  //     }
-  //   } catch (error) {
-  //     console.log("클릭 카운트 업데이트 에러:", error);
-  //   }
-  // };
-
-  // useEffect(() => {
-  //   const startInterval = () => {
-  //     const newIntervalId = setInterval(() => {
-  //       sendClickCountsToBackend();
-  //     }, 600000); // 600초 (10분)
-
-  //     setIntervalId(newIntervalId);
-  //   };
-
-  //   // 클릭 타임스탬프가 있는 경우에만 주기적인 업데이트 시작
-  //   if (Object.keys(lastClickTimes).length > 0) {
-  //     startInterval();
-  //   } else if (intervalId) {
-  //     clearInterval(intervalId);
-  //   }
-
-  //   return () => {
-  //     if (intervalId) {
-  //       clearInterval(intervalId);
-  //     }
-  //     sendClickCountsToBackend(); // 컴포넌트 언마운트 시 마지막 업데이트 수행
-  //   };
-  // }, [lastClickTimes]);
 
   const renderItems = () => {
     const rows = [];
@@ -98,13 +32,7 @@ export default function MarketItems({ itemsPerPage, itemsToShow }: ItemsProps) {
           } `}
         >
           {rowItems.map((item: Product) => (
-            <Markeitem
-              key={item.productId}
-              item={item}
-              // updateClickCount={updateClickCount}
-              // lastClickTime={lastClickTimes[item.productId] || null}
-              // updateLastClickTime={updateLastClickTime} // setLastClickTime 함수 전달
-            />
+            <Markeitem key={item.productId} item={item} />
           ))}
         </div>
       );
