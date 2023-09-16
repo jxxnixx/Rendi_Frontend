c4452fd6e00ac3a209a13056933cca4d6a65e9c8
sen2y <pse6704@naver.com>
2023-09-16 20:17:23 +0900

    trend page 추가

--- components/structure/banner.tsx
+++ components/structure/banner.tsx
@@ -30,29 +30,6 @@ export default function Banner() {
             className="w-[1040px] h-[362px] mobile:w-full mobile:h-[226px] object-cover"
           />
         </div>
-        <div>
-          <div className="flex justify-center items-center w-[1040px] h-[362px] relative overflow-hidden bg-[#ffe9ec]/90 mobile:w-full mobile:h-[226px]">
-            <div className="flex justify-center items-center w-[1000px] h-[287px] ">
-              <div className=" h-[186px]  flex-col">
-                <p className="text-[18px] text-[#666666] font-medium">
-                  Rendi에 처음 오셨나요?
-                </p>
-                <p className="text-[45px] font-bold  text-shadow-[0px 4px 4px rgba(0,0,0,0.25) mobile:w-full mobile:h-[226px]">
-                  test page
-                </p>
-                <img
-                  className="w-[100px] absolute right-[60px] top-[30px]"
-                  src="logo.png"
-                  alt="logo Image"
-                />
-              </div>
-              <div className="flex-1 h-[186px] bg-black"></div>
-            </div>
-          </div>
-        </div>
-        <div className="flex justify-center items-center w-[1040px] h-[362px] relative overflow-hidden bg-orange-200 mobile:w-full mobile:h-[226px]">
-          <h1 className="color-white line-height-[362px]">slide1</h1>
-        </div>
       </Carousel>
     </div>
   );
